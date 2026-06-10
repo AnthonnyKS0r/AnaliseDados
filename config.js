4fbf2efab1c4864001dad58dfcd123ff36b34ca4
AnthonnyKS0r <anthonny.goncalvess@gmail.com>
2026-06-10 10:11:08 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const SUPABASE_URL = 'https://cjtbqzcbzrxkddndobfu.supabase.co';
-export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqdGJxemNienJ4a2RkbmRvYmZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NDc3MzIsImV4cCI6MjA5NDIyMzczMn0.agrY1BcpmHGuX7Dp_QkD_2s5RZ0AhD1B6YVpAuccihA';
+export const SUPABASE_URL =  'https://nksvfifsnobyinjolhhc.supabase.co';
+export const SUPABASE_KEY =  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5rc3ZmaWZzbm9ieWluam9saGhjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwNzY1NzMsImV4cCI6MjA5NjY1MjU3M30.tX-W3TVWpMhW8IMXn17uX5apihMdV5gIs93srSHbpv4';
